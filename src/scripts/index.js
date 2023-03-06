79e754a072a4d441faf7f43af45dda13763bfec2
jefitlopes <jefitlopes@gmail.com>
2023-03-06 09:26:07 -0300

    feat: general update2

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,4 +1,4 @@
-import { toast } from "./render.js";
+import { createToast } from "./render.js";
 import { getUserProfile, login } from "./request.js";
 
 function authentication() {
@@ -31,7 +31,7 @@ export function handleLogin (){
             count = 0;
             inputEmail.value = '';
             inputPassword.value = '';
-            toast('Informe os dados', 'Por favor, forneça o email e senha de login', '', 'redLogin');
+            createToast('Informe os dados', 'Por favor, forneça o email e senha de login', '', 'redLogin');
             return;
         } else {
             const token = await login(loginBody);
@@ -43,95 +43,6 @@ export function handleLogin (){
     })
 }
 
-// export function handleGoToRegister (){
-//     const buttonRegister = document.querySelector('.btn__register');
-
-//     buttonRegister.addEventListener('click', (event) =>{
-//         event.preventDefault();
-//         window.location.replace('./src/pages/register.html');
-//     })
-// }
 
 authentication();
 handleLogin();
-// handleGoToRegister();
-
-
-
-
-
-
-// import { loginRequest } from "./request.js";
-
-// function authentication(){
-//     const token = localStorage.getItem('@doit:token')
-
-//     if(token){
-//         window.location.replace('./src/pages/dashboard.html')
-//     }
-// }
-
-// const createPage = () => {
-//     document.body.insertAdjacentHTML('beforeend', `
-//     <main>
-//         <div class="site__introduction">
-//             <h1 class="site__title">PetInfo</h1>
-//             <h2 class="site__subtitle">Amor e carinho por meio do conhecimento</h2>
-//             <p class="site__text">Todas as informações para melhorar a vida do seu pet em um só lugar</p>
-//             <div class="site__img">
-//                 <img src="./src/assets/img/cat_img1.png" alt="cat looking">
-//                 <img src="./src/assets/img/dog_img1.png" alt="dog looking">
-//                 <img src="./src/assets/img/cat_img2.png" alt="cat impressed">
-//                 <img src="./src/assets/img/dog_img2.png" alt="dog layed down">
-//             </div>
-//         </div>
-//         <div class="login__container">
-//             <form action="" class="login__form">
-//                 <h2>login</h2>
-//                 <div>
-//                     <label for="">Email</label>
-//                     <input type="email" name="email" id="email" placeholder="Digite seu email aqui">
-//                     <label for="">Senha</label>
-//                     <input type="password" name="password" id="password" placeholder="Digite sua senha aqui">
-//                     <button class="login__btn">Acessar</button>
-//                 </div>
-//                 <p class="create__title">Ainda não possui conta?</p>
-//                 <p class="create__text">Clicando no botão abaixo, você pode se cadastrar rapidamente</p>
-//                 <a href="./src/pages/register.html" class="register__btn">Cadastrar</a>
-//             </form>
-//         </div>
-//     </main>
-//     `) 
-//  }
-//  createPage()
-
-//  function handleLogin(){
-//     const inputs = document.querySelectorAll('input');
-//     const button = document.querySelector('.login__btn');
-//     const loginBoody = {};
-//     let count = 0;
-
-//     button.addEventListener('click', async (event) => {
-//         event.preventDefault()
-
-//         inputs.forEach(({name, value}) => {
-//             if(value === ''){
-//                 count++
-//             }
-//             loginBoody[name] = value;
-//         })
-//         if (count !== 0){
-//             return alert('Por favor preencha os campos e tente novamente')
-//         } else{
-//             const token = await loginRequest(loginBoody)
-
-//             setTimeout(() => {
-//                 window.location.replace('./src/pages/dashboard.html')
-//             }, 1000);
-
-//             return token;
-//         }
-//     });
-//  }
-//  handleLogin()
-//  authentication()
--- src/scripts/request.js
+++ src/scripts/request.js
@@ -1,4 +1,4 @@
-import { renderProfilePhoto, toast, showLogout } from "./render.js";
+import { renderProfilePhoto, createToast, showLogout } from "./render.js";
 
 const baseURL = 'http://localhost:3333';
 const token = JSON.parse(localStorage.getItem("@petInfo:token")) || "";
@@ -40,7 +40,7 @@ export async function createUser (createBody){
         body: JSON.stringify(createBody)
     }).then((response)=>{
         if(response.ok){
-            toast('Sua conta foi criada com sucesso!', 'Agora você pode acessar os conteúdos utilizando seu usuário e senha na página de login: ', "../../index.html" )
+            createToast('Sua conta foi criada com sucesso!', 'Agora você pode acessar os conteúdos utilizando seu usuário e senha na página de login: ', "../../index.html" )
             
             setTimeout(() => {
                 window.location.replace('../../index.html');
@@ -71,7 +71,7 @@ export async function updatePost (postBody, postId){
         body: JSON.stringify(postBody)
     }).then((response) =>{
         if(response.ok){
-            toast('Post editado com sucesso!','O post agora contém as novas informações passadas');
+            createToast('Post editado com sucesso!','O post agora contém as novas informações passadas');
             return response.json();
         } else {
             response.json().then((responseError) =>{
@@ -88,7 +88,7 @@ export async function deletePost (postId){
         headers: requestHeaders
     }).then((response) =>{
         if(response.ok){
-            toast('Post deletado com sucesso!','O post selecionado para exlusão foi deletado, a partir de agora não aparecerá no seu feed');
+            createToast('Post deletado com sucesso!','O post selecionado para exlusão foi deletado, a partir de agora não aparecerá no seu feed');
             
             return response.json();
         } else {
@@ -107,7 +107,7 @@ export async function createPost (postBody){
         body: JSON.stringify(postBody)
     }).then((response)=>{
         if(response.ok){
-            toast('Post criado com sucesso!','Seu post foi criado com sucesso, a partir de agora ele aparecerá no seu feed')
+            createToast('Post criado com sucesso!','Seu post foi criado com sucesso, a partir de agora ele aparecerá no seu feed')
 
             return response.json();
         } else {
@@ -142,203 +142,3 @@ export async function createPost (postBody){
         
             return userProfile;
         }
-
-
-
-
-// const token = localStorage.getItem('@doit:token')
-// const baseUrl = 'http://localhost:3333'
-// const requestHeaders = {
-//     'Content-Type': 'application/json',
-//     Authorization: `Bearer ${token}`
-// }
-
-// export async function loginRequest(loginBody){
-//     const token = await fetch(`${baseUrl}/login`, {
-//         method: 'POST',
-//         headers: requestHeaders,
-//         body: JSON.stringify(loginBody)
-//     })
-//     .then((response) => {
-//         if(response.ok){
-//             const responseJson = response.json().then(({token}) => {
-//              localStorage.setItem('@doit:token', JSON.stringify(token))
-//             return token   
-//             })
-//             return responseJson
-
-//         } else{
-//             response.json().then(resError => console.log(resError))
-//         }
-//     })
-//     return token
-// }
-
-// export async function registerRequest(registerBody){
-//     const newUser = await fetch(`${baseUrl}/users/create`, {
-//         method: 'POST',
-//         headers: requestHeaders,
-//         body: JSON.stringify(registerBody)
-//     })
-//     .then(response => {
-//         if(response.ok){
-//             return response.json()
-//         } else{
-//             response.json().then(resError => console.log(resError))
-//         }
-//     })
-//     return newUser
-// }
-
-// export async function getProfile (){
-//     const userProfile = await fetch(`${baseUrl}/users/profile`, {
-//         method: "GET",
-//         headers: requestHeaders
-//     }).then((response) =>{
-//         if(response.ok){
-//             const userData = response.json().then((responseJson) =>{
-//                 localStorage.setItem(
-//                     "@petInfo:data",
-//                     JSON.stringify(responseJson)
-//                 )
-                
-//                 return responseJson;
-//                }).then(async(res) =>{
-//                 await renderPerfilPhoto();
-//                 showLogout();                       
-//                })
-//         } else {
-//             response.json().then((responseError) =>{
-//             })
-//         }
-//     })
-
-//     return userProfile;
-// }
-
-// export async function creatPost(postBody){
-//     const newPost = await fetch(`${baseUrl}/posts/create`,{
-//         method: 'POST',
-//         headers: requestHeaders,
-//         body: JSON.stringify(postBody)
-//     })
-//     .then((response) => {
-//         if(response.ok) {
-//             const postJson = response.json().then(resJson => {
-//                 alert('Post criado com sucesso')
-
-//                 return postJson
-//             })
-//             return postJson
-//         } else{
-//             response.json().then(({message}) => 
-//             alert(message)
-//             )
-//         }
-//     })
-// }
-
-// // // Função assíncrona para criar um post
-// // async function createPost(token, postContent) {
-// //     // Define o cabeçalho da requisição com o token de autorização
-// //     const headers = {
-// //       'Authorization': `Bearer ${token}`
-// //     };
-    
-// //     // Define o corpo da requisição com o conteúdo do post
-// //     const data = {
-// //       'content': postContent
-// //     };
-    
-// //     try {
-// //       // Faz a requisição usando o método POST
-// //       const response = await fetch(`${baseUrl}/posts/create`, {
-// //         method: 'POST',
-// //         headers: headers,
-// //         body: JSON.stringify(data)
-// //       });
-      
-// //       // Verifica se a requisição foi bem-sucedida
-// //       if (!response.ok) {
-// //         throw new Error('Erro ao criar o post');
-// //       }
-      
-// //       // Retorna a resposta em formato JSON
-// //       const responseData = await response.json();
-      
-// //       // Processa a resposta
-// //       console.log('Post criado com sucesso');
-// //       console.log(responseData);
-// //     } catch (error) {
-// //       // Trata erros
-// //       console.error(error);
-// //     }
-// //   }
-  
-// //   // Chama a função de criar post ao clicar no botão de enviar
-// //   document.getElementById('enviar').addEventListener('click', function() {
-// //     // Obtém o token do usuário da sessão
-// //     const token = sessionStorage.getItem('token');
-    
-// //     // Obtém o conteúdo do post
-// //     const postContent = document.getElementById('post-content').value;
-    
-// //     // Cria o post
-// //     createPost(token, postContent);
-// //   });
-  
-  
-
-// export async function readAllPost(){
-//     const posts = await fetch (`${baseUrl}/posts`, {
-//     method: 'GET',
-//     headers: requestHeaders,
-//     })
-//     .then(response => {
-//         if(response.ok){
-//             return response.json()
-//         } else{
-//             response.json().then(({message}) => {
-//                 alert(message)
-//             })
-//         }
-//     })
-//     return posts;
-// }
-
-// export async function postUpdate (postBody, postId){
-//     const update = await fetch(`${baseUrl}/posts/${postId}`, {
-//         method: "PATCH",
-//         headers: requestHeaders,
-//         body: JSON.stringify(postBody)
-//     }).then((response) =>{
-//         if(response.ok){
-//             toast('Post editado com sucesso!','O post agora contém as novas informações passadas');
-//             return response.json();
-//         } else {
-//             response.json().then((responseError) =>{
-//             })
-//         } 
-//     })
-
-//     return update;
-// }
-
-// export async function deletePost(postId){
-//     const del = await fetch(`${baseUrl}/posts/${postId}`, {
-//         method: "DELETE",
-//         headers: requestHeaders
-//     })
-//     .then(response => {
-//         if(response.ok){
-//             toast('Post deletado com sucesso!')
-
-//             return response.json()
-//         } else{
-//             response.json().then(({message}) => {
-//                 alert(message);
-//             })
-//         }
-//     })
-//     return del 
-// }
